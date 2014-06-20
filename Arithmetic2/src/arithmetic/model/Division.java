289f66aaebcfc606fddc680534f3bc4a0fd0294a
yohansugiyo <yohan.ultima99@gmail.com>
2014-06-20 10:59:49 +0700

    nih udh gw ganti. banyak mau lu

--- Arithmetic2/src/arithmetic/model/Division.java
+++ Arithmetic2/src/arithmetic/model/Division.java
@@ -1,15 +1,15 @@
 package arithmetic.model;
 
-public class Multiplication implements ArithmeticOperation {
+public class Division implements ArithmeticOperation {
 
 	@Override
 	public double compute(double a, double b) {
-		return a * b;
+		return a / b;
 	}
 
 	@Override
 	public char getSign() {
-		return '*';
+		return '/';
 	}
 
 }
